e972e939e3c41d5365582297794e58dd940abd69
flavian-anselmo <anselmoflavian4@gmail.com>
2021-09-22 11:15:52 +0300

    finished with the models

--- legofiHackathon/quizler/models.py
+++ legofiHackathon/quizler/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    std_name=models.CharField(max_length=100)
+    phone=models.CharField(max_length=100)
+class Language(models.Model):
+    c=models.BooleanField('C',default=False)
+    cpp=models.BooleanField('C++',default=False)
+    ruby=models.BooleanField('Ruby on Rails',default=False)
+    perl=models.BooleanField('Perl',default=False)
+    java=models.BooleanField('Java',default=False)
+    asp=models.BooleanField('ASP',default=False) 
